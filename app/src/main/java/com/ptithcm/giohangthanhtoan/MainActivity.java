efd60befe79e088a4bb4bbc41ab96cf8ae785ec6
dat.vt <dat.vt@geekup.vn>
2023-04-14 14:58:10 +0700

    Push code

---
+++ app/src/main/java/com/ptithcm/giohangthanhtoan/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ptithcm.giohangthanhtoan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gio_hang);
+        //
+    }
+}
